4652710fc40d1a1d5a23f143f89b05f55641e48b
unknown <m.abdullahshahid2003@gmail.com>
2025-10-21 11:38:13 +0500

    hello

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flask-lab-project',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'Flask==2.3.4',
+    ],
+    extras_require={
+        'test': [
+            'pytest==7.4.3',
+            'pytest-cov==4.1.0',
+        ],
+    },
+)
